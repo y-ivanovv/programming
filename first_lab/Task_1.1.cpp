c3bdec145832fcecf7beacb52f9abdc76fcbebf2
yurbanchik <151701038+yurbanchik@users.noreply.github.com>
2024-04-08 09:22:09 +0300

    Add files via upload

---
+++ first_lab/Task_1.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	stack<int> st;
+	int n;
+	cout << "N = ";
+	cin >> n;
+	cout << "Введите " << n << " чисел: ";
+	for (int i = 0; i < n; i++)
+	{
+		int a;
+		cin >> a;
+		st.push(a);
+	}
+	cout << "Вершина стека - " << st.top();
+	return 0;
+}
