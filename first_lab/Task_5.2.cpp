567a3c4d009bdf7dad7011d074b747e3c2167f2b
yurbanchik <151701038+yurbanchik@users.noreply.github.com>
2024-04-08 09:40:25 +0300

    Add files via upload

---
+++ first_lab/Task_5.2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream> 
+#include <list> 
+#include <string> 
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	list<char> l;
+	string s;
+	cout << "Введите символы: ";
+	cin >> s;
+	for (int i = 0; i < s.size(); i++)
+	{
+		l.push_back(s[i]);
+	}
+	auto it = l.begin();
+	while (it != l.end())
+	{
+		if (*it == '&')
+		{
+			if (next(it) != l.end())
+			{
+				it = l.erase(next(it));
+			}
+			else
+			{
+				break;
+			}
+		}
+		else
+		{
+			it++;
+		}
+	}
+	for (char& elem : l)
+	{
+		cout << elem << " ";
+	}
+	return 0;
+}
