7e00eb6d2b4e52927d3b1284cc9930bcf2fccff2
yurbanchik <151701038+yurbanchik@users.noreply.github.com>
2024-04-08 09:24:40 +0300

    Add files via upload

---
+++ first_lab/Task_1.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	queue<double> que;
+	int n, neg = 0;
+	cout << "N = ";
+	cin >> n;
+	cout << "Введите " << n << " чисел: ";
+	for (int i = 0; i < n; i++)
+	{
+		double a;
+		cin >> a;
+		que.push(a);
+		if (a < 0)
+		{
+			neg++;
+		}
+	}
+	cout << "Кол-во отрицательных элементов: " << neg << endl;
+	cout << "Данные очереди:" << endl;
+	while (!que.empty())
+	{
+		cout << que.front() << " ";
+		que.pop();
+	}
+	return 0;
+}
