40432bd0c8f8482267adb15ae0f0ad84331236ac
yurbanchik <151701038+yurbanchik@users.noreply.github.com>
2024-04-08 09:38:53 +0300

    Add files via upload

---
+++ first_lab/Task_5.1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <forward_list>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	forward_list<float> l;
+	int n;
+	cout << "N = ";
+	cin >> n;
+	cout << "Введите целые числа: ";
+	for (int i = 0; i < n; i++)
+	{
+		float num;
+		cin >> num;
+		l.push_front(num);
+		if (num < 0)
+		{
+			l.push_front(1.5);
+		}
+	}
+	l.reverse();
+	cout << "Ваш новый список: ";
+	for (float& elem : l)
+	{
+		cout << elem << " ";
+	}
+	return 0;
+}
