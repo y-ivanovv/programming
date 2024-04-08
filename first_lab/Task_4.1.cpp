c962dd386d13a131ee0d052ce773382c6ababe12
yurbanchik <151701038+yurbanchik@users.noreply.github.com>
2024-04-08 09:34:15 +0300

    Add files via upload

---
+++ first_lab/Task_4.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <forward_list>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	forward_list<float> l;
+	int n, count = 0;
+	cout << "N = ";
+	cin >> n;
+	cout << "Введите вещественные числа: ";
+	for (int i = 0; i < n; i++)
+	{
+		float num;
+		cin >> num;
+		l.push_front(num);
+		if (num > 7)
+		{
+			count++;
+		}
+	}
+	cout << "Количество элементов списка со значениями больше 7: " << count;
+	return 0;
+}
