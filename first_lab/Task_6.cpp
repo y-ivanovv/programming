5e271f52ecac1603d731d4b8efba336c246d1b92
yurbanchik <151701038+yurbanchik@users.noreply.github.com>
2024-04-08 09:41:56 +0300

    Add files via upload

---
+++ first_lab/Task_6.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	list<int> l;
+	int n;
+	cout << "N = ";
+	cin >> n;
+	cout << "Введите ваши числа: ";
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		l.push_back(num);
+	}
+	l.reverse();
+	for (auto it = l.begin(); it != l.end(); it++)
+	{
+		if (*it < 15)
+		{
+			l.erase(it);
+			break;
+		}
+	}
+	l.reverse();
+	cout << "Новый список: ";
+	for (int& elem : l)
+	{
+		cout << elem << " ";
+	}
+	return 0;
+}
