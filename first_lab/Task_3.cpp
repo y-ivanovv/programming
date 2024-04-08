10ad1d820a7bd707a1caabc2de86e846dd44d837
yurbanchik <151701038+yurbanchik@users.noreply.github.com>
2024-04-08 09:31:54 +0300

    Add files via upload

---
+++ first_lab/Task_3.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	string s;
+	cin >> s;
+	n = s.size();
+	stack<char> st;
+	for (int i = 0; i < n; i++)
+	{
+		if (s[i] == '(' or s[i] == '[' or s[i] == '{')
+		{
+			st.push(s[i]);
+		}
+		else
+		{
+			if (st.size() == 0)
+			{
+				cout << "No";
+				return 0;
+			}
+			if (s[i] == ')' and st.top() == '(')
+			{
+				st.pop();
+			}
+			else if (s[i] == ']' and st.top() == '[')
+			{
+				st.pop();
+			}
+			else if (s[i] == '}' and st.top() == '{')
+			{
+				st.pop();
+			}
+			else
+			{
+				cout << "No";
+				return 0;
+			}
+		}
+	}
+	if (st.size() == 0)
+	{
+		cout << "Yes";
+	}
+	else
+	{
+		cout << "No";
+	}
+	return 0;
+}
