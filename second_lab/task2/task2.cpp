817fa61edc9cfd8bfb1bccb554e8472bb2660c16
yurbanchik <151701038+yurbanchik@users.noreply.github.com>
2024-05-26 16:26:38 +0300

    Add files via upload

---
+++ second_lab/task2/task2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	
+	ifstream file_1("a.txt");
+	ifstream file_2("b.txt");
+
+	if (!file_1.is_open() or !file_2.is_open())
+	{
+		cerr << "Ошибка открытия файла" << endl;
+		return 1;
+	}
+
+	int count = 0;
+	double num;
+
+	while (file_1 >> num)
+	{
+		if (num == 0)
+		{
+			count++;
+		}
+	}
+	while (file_2 >> num)
+	{
+		if (num == 0)
+		{
+			count++;
+		}
+	}
+
+	file_1.close();
+	file_2.close();
+	
+	cout << "Кол-во нулевых элементов: " << count << endl;
+
+	return 0;
+}
