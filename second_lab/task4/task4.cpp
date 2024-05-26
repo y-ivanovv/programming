e9202593b9dbf9a53b326b6fe7ef23dc6d052841
yurbanchik <151701038+yurbanchik@users.noreply.github.com>
2024-05-26 16:28:55 +0300

    Add files via upload

---
+++ second_lab/task4/task4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	
+	ifstream file("a.txt");
+	
+	if (!file.is_open())
+	{
+		cerr << "Не удалось открыть файл" << endl;
+		return 1;
+	}
+
+	int num;
+	int index = 1;
+	int max_number = -125;
+	while (file >> num)
+	{
+		if (index % 3 == 0 && num > max_number)
+		{
+			max_number = num;
+		}
+		index++;
+	}
+
+	file.close();
+
+	if (max_number == -125)
+	{
+		cout << "В файле нет элементов с индексами, кратными трем" << endl;
+	}
+	else
+	{
+		cout << "Наибольший элемент файла среди элементов, номера которых кратны трем: " << max_number << endl;
+	}
+
+	return 0;
+}
